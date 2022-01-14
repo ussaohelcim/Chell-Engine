80a12886b9a86dad86cacb60599a1f2a1cd0463d
ussaohelcim <michel.darkshadow@live.com>
2022-01-14 18:13:41 -0300

    adicionado para buildar pro linux

--- build_stuff/buildeverything.ps1
+++ build_stuff/buildeverything.ps1
@@ -1,13 +1,20 @@
 #dotnet publish -r linux-x64
 
+#$op = Read-Host "'l': linux64, 'w': windows64"
+
 dotnet publish "..\engine\" --output ".\temp\"
 
 Write-Host "Creating ChellEngine folder..."
 New-Item "ChellEngine" -ItemType Directory
 
-Copy-Item "..\raylib\raylib.dll" -Destination ".\ChellEngine\"
-Copy-Item ".\temp\engine.dll" -Destination ".\ChellEngine\"
-Copy-Item ".\temp\Raylib-cs.dll" -Destination ".\ChellEngine\"
+#Copy-Item "..\raylib\raylib.dll" -Destination ".\ChellEngine\"
+#Copy-Item ".\temp\engine.dll" -Destination ".\ChellEngine\"
+#Copy-Item ".\temp\Raylib-cs.dll" -Destination ".\ChellEngine\"
+
+Copy-Item "..\raylib\linux64\libraylib.so" -Destination ".\ChellEngine\"
+Copy-Item "..\raylib\linux64\libraylib.so.3.7.0" -Destination ".\ChellEngine\"
+Copy-Item "..\raylib\linux64\libraylib.so.370" -Destination ".\ChellEngine\"
+Copy-Item "..\raylib\windows64\raylib.dll" -Destination ".\ChellEngine\"
 
 Remove-Item ".\temp\" -Force -Recurse
 
